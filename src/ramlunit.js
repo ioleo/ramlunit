d314e2624fe9d8d46fba11e1cb0008917114d7f6
Piotr Gołębiewski <loostro@gmail.com>
2017-02-23 12:25:49 +0100

    Ensure output file exists

--- src/ramlunit.js
+++ src/ramlunit.js
@@ -2,6 +2,7 @@
 
 var path = require('path'),
     cli = require('commander'),
+    fs = require('fs-extra'),
     jsonfile = require('jsonfile'),
     Linter = require('ramllint'),
     XMLWriter = require('xml-writer'),
@@ -19,43 +20,57 @@ cli.input = path.resolve(cli.args[0] || 'main.raml');
 cli.config = path.resolve(cli.config || '.ramlunit');
 cli.output = path.resolve(cli.output || 'report.xml');
 
+fs.ensureFile(cli.output, function(err) {
+    if (err) console.log(err);
+    else main();
+});
+
 function linterOptions() {
     console.log('Config read from ' + cli.config);
     try { return jsonfile.readFileSync(cli.config); }
     catch (ex) { return {}; }
 }
 
-var fileWriter = new SimpleFileWriter(cli.output, { flags: 'w' }),
-    ramlLinter = new Linter(linterOptions()),
-    xmlBuilder = new XMLWriter(true);
+function main() {
+    var fileWriter = new SimpleFileWriter(cli.output, { flags: 'w' }),
+        ramlLinter = new Linter(linterOptions()),
+        xmlBuilder = new XMLWriter(true);
 
-ramlLinter.lint(cli.input, function outputFn() {
-    'use strict';
+    ramlLinter.lint(cli.input, function outputFn() {
+        'use strict';
 
-    var success = ramlLinter.results('success');
-    var errors = ramlLinter.results('error');
-    var tests = success + errors;
-    var exitcode = (!errors.length) ? 0 : 1;
+        var success = ramlLinter.results('success');
+        var errors = ramlLinter.results('error');
+        var tests = success + errors;
+        var exitcode = (!errors.length) ? 0 : 1;
 
-    console.log('Running ' + tests.length + ' tests...');
-    console.log('Found ' + errors.length + ' failures');
-    xmlBuilder.startDocument('1.0', 'UTF-8');
-    xmlBuilder.startElement('testsuite');
-    xmlBuilder.writeAttribute('tests', tests.length);
-    xmlBuilder.writeAttribute('failures', errors.length);
+        console.log('Running ' + tests.length + ' tests...');
+        console.log('Found ' + errors.length + ' failures');
+        xmlBuilder.startDocument('1.0', 'UTF-8');
+        xmlBuilder.startElement('testsuite');
+        xmlBuilder.writeAttribute('name', 'RAML Linter Suite');
+        xmlBuilder.writeAttribute('tests', tests.length);
+        xmlBuilder.writeAttribute('failures', errors.length);
+        xmlBuilder.startElement('testcase');
+        xmlBuilder.writeAttribute('name', 'Check for errors');
 
-    errors.forEach(function entryFormat(entry) {
-        xmlBuilder.startElement('failure');
-        xmlBuilder.writeAttribute('type', entry.rule);
-        xmlBuilder.writeAttribute('message', entry.message);
+        errors.forEach(function entryFormat(entry) {
+            xmlBuilder.startElement('failure');
+            xmlBuilder.writeAttribute('type', entry.rule);
+            xmlBuilder.writeAttribute('message', entry.message);
+            xmlBuilder.endElement();
+            console.log('At: ' + entry.rule);
+            console.log('    ' + entry.message);
+        });
+
         xmlBuilder.endElement();
-        console.log('At: ' + entry.rule);
-        console.log('    ' + entry.message);
+        xmlBuilder.endElement();
+        xmlBuilder.endDocument();
+        fileWriter.write(xmlBuilder.toString(), function(err) {
+            if (err) console.log(err);
+            else console.log('Report saved to ' + cli.output);
+        });
+        process.exit(exitcode);
     });
 
-    xmlBuilder.endElement();
-    xmlBuilder.endDocument();
-    fileWriter.write(xmlBuilder.toString());
-    console.log('Report saved to ' + cli.output);
-    process.exit(exitcode);
-});
+}
